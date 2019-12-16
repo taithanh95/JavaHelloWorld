351520fc963ee0ebe2d9e7dfbf64b03177fb84e4
taithanh95 <58700144+taithanh95@users.noreply.github.com>
2019-12-16 14:46:28 +0700

    Thanh.PTBacHai

---
+++ PTBacHai/src/com/bof/ptbachai/PTBacHai.java
@@ -0,0 +1,31 @@
+package com.bof.ptbachai;
+
+public class PTBacHai {
+    private double a, b, c, x1, x2, delta;
+
+    public void nhapGT(double a0, double b0, double c0){
+        a= a0;
+        b= b0;
+        c= c0;
+    }
+
+    public void tinhDelta(){
+        delta= b*b - 4*a*c;
+    }
+    public void timNghiem(){
+        if (delta < 0){
+            System.out.println("Phuong trinh vo nghiem");
+        } else if(delta == 0){
+            x1= -b/2*a;
+            x1=x2;
+            System.out.println("Phuong trinh co nghiem x1=x2 ="+x1);
+        }else if(delta > 0){
+            x1= (-b+ Math.sqrt(delta))/2*a;
+            x2= (-b- Math.sqrt(delta))/2*a;
+            inKQ();
+        }
+    }
+    public inKQ(){
+        System.out.println("Nghiem so 1: "+ x1 + "\n" "Nghiem so 2:" +x2);
+    }
+}
---
+++ PTBacHai/src/com/bof/ptbachai/main/Main.java
@@ -0,0 +1,12 @@
+package com.bof.ptbachai.main;
+
+import com.bof.ptbachai.PTBacHai;
+
+public class Main {
+    public static void main(String[] args) {
+        PTBacHai pt = new PTBacHai();
+        pt.nhapGT(2,1,0);
+        pt.tinhDelta();
+        pt.timNghiem();
+    }
+}
